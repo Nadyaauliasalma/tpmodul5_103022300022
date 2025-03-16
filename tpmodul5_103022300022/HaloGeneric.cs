47d774b03da1e84b175167f0f02ee390c1b42e81
Nadya Aulia <nadyaauliasalma622@gmail.com>
2025-03-16 16:45:13 +0700

    menambahkan class DataGeneric

---
+++ tpmodul5_103022300022/HaloGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tpmodul5_103022300022;
+
+namespace tpmodul5_103022300022
+{
+    public class HaloGeneric  
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine("Halo user " + user);
+        }
+    }
+}
--- tpmodul5_103022300022/Program.cs
+++ tpmodul5_103022300022/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul5_103022300022
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataGeneric<string> dataNIM = new DataGeneric<string>("103022300022");
+
+            dataNIM.PrintData();
+        }
+    }
+}
